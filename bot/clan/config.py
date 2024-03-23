7ba7aa0228976fd75faff8a8f04c79038490f515
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-03-23 18:00:08 -0500

    initial

--- bot/clan/config.py
+++ bot/clan/config.py
@@ -1,8 +1,9 @@
-from utility.config import Config
+from utility.config import Config, master_api_config
 from dataclasses import dataclass
 
 @dataclass
 class BotClanTrackingConfig(Config):
-    min_coc_email = 41
-    max_coc_email = 45
+    min_coc_email = master_api_config.get("bot_clan")[0]
+    max_coc_email = master_api_config.get("bot_clan")[1]
     redis_max_connections = 250
+
--- gamewide/war/config.py
+++ gamewide/war/config.py
@@ -3,12 +3,11 @@ from dataclasses import dataclass
 
 
 @dataclass
-class BotPlayerTrackingConfig(Config):
-    min_coc_email = master_api_config.get("bot_player")[0]
-    max_coc_email = master_api_config.get("bot_player")[1]
+class LegendTrackingConfig(Config):
+    min_coc_email = master_api_config.get("global_war")[0]
+    max_coc_email = master_api_config.get("global_war")[1]
     redis_max_connections = 2500
 
     secondary_loop_change = 15
     tertiary_loop_change = 150
     max_tag_split = 50_000
-
