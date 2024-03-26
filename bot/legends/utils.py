ef5c4382399d16b72e7d3d056292602f5803298a
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-03-25 22:06:20 -0500

    fix: wrong spelling

--- bot/legends/utils.py
+++ bot/legends/utils.py
@@ -15,28 +15,27 @@ from utility.classes import MongoDatabase
 from utility.utils import gen_season_date, gen_raid_date, gen_games_season, gen_legend_date
 from asyncio_throttle import Throttler
 
-throttler = Throttler(rate_limit=1200, period=1)
 
 async def get_player_responses(keys: deque, tags: list[str]):
+    throttler = Throttler(rate_limit=1200, period=1)
     tasks = []
     connector = aiohttp.TCPConnector(limit=500, ttl_dns_cache=300)
     timeout = aiohttp.ClientTimeout(total=1800)
     session = aiohttp.ClientSession(connector=connector, timeout=timeout)
     for tag in tags:
         keys.rotate(1)
-
-        async def fetch(url, session: aiohttp.ClientSession, headers):
+        async def fetch(url, session: aiohttp.ClientSession, headers, throttler: Throttler):
             async with throttler:
-                new_response = await session.get(url, headers=headers)
-                t = f'#{url.split("%23")[-1]}'
-                if new_response.status == 404:  # remove banned players
-                    return (t, "delete")
-                elif new_response.status != 200:
-                    return (t, None)
-                new_response = await new_response.read()
-                return (t, new_response)
+                async with session.get(url, headers=headers) as response:
+                    t = f'#{url.split("%23")[-1]}'
+                    if response.status == 404:  # remove banned players
+                        return (t, "delete")
+                    elif response.status != 200:
+                        return (t, None)
+                    new_response = await response.read()
+                    return (t, new_response)
 
-        tasks.append(fetch(url=f'https://api.clashofclans.com/v1/players/{tag.replace("#", "%23")}', session=session, headers={"Authorization": f"Bearer {keys[0]}"}))
+        tasks.append(fetch(url=f'https://api.clashofclans.com/v1/players/{tag.replace("#", "%23")}', session=session, headers={"Authorization": f"Bearer {keys[0]}"}, throttler=throttler))
 
     results = await asyncio.gather(*tasks, return_exceptions=True)
     await session.close()
