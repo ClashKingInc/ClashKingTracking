6ccbbfcdc3526a8168af602cdd9e1d0ae5c1ba58
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-04-02 22:32:09 -0500

    fix: meilisearch size

--- gamewide/search/track.py
+++ gamewide/search/track.py
@@ -4,40 +4,52 @@ from utility.classes import MongoDatabase
 from meilisearch_python_sdk import AsyncClient
 from utility.config import Config
 from loguru import logger
-
+import orjson
+import snappy
 import aiohttp
+from redis import asyncio as redis
+import pendulum as pend
+import time
 
 async def main():
     config = Config()
     db_client = MongoDatabase(stats_db_connection=config.stats_mongodb, static_db_connection=config.static_mongodb)
 
-    pipeline = [{"$match": {"$nor": [{"members": {"$lt": 10}}, {"level": {"$lt": 3}}, {"capitalLeague": "Unranked"}]}}, {"$group": {"_id": "$tag"}}]
-    all_tags = [x["_id"] for x in (await db_client.global_clans.aggregate(pipeline).to_list(length=None))]
-    bot_clan_tags = await db_client.clans_db.distinct("tag")
-    all_tags = list(set(all_tags + bot_clan_tags))
-    logger.info(f"{len(all_tags)} tags")
-    size_break = 100_000
-    all_tags = [all_tags[i:i + size_break] for i in range(0, len(all_tags), size_break)]
+    cache = redis.Redis(host=config.redis_ip, port=6379, db=0, password=config.redis_pw, decode_responses=False, max_connections=50,
+                        health_check_interval=10, socket_connect_timeout=5, retry_on_timeout=True, socket_keepalive=True)
 
-    for tag_group in all_tags:
-        pipeline = [
-            {"$match": {"tag": {"$in": tag_group}}},
-            {"$unwind": "$memberList"},
-            {
-                "$project": {
-                    "id": {"$substr": ["$memberList.tag", 1, {"$strLenBytes": "$memberList.tag"}]},
-                    "name": "$memberList.name",
-                    "clan_name": "$name",
-                    "clan_tag": "$tag",
-                    "townhall": "$memberList.townhall"
+    # change to find changes in last 20 mins
+    current_time = pend.now(tz=pend.UTC)
+    time_20_mins_ago = current_time.subtract(minutes=20).timestamp()
+    # any players that had an update in last 20 mins
+    pipeline = [{"$match": {}},
+                {"$project": {"tag": "$tag"}},
+                {"$unset": "_id"}]
+    all_player_tags = [x["tag"] for x in (await db_client.player_stats.aggregate(pipeline).to_list(length=None))]
+    # delete any tags that are gone
+    # await db_client.player_autocomplete.delete_many({"tag" : {"$nin" : all_player_tags}})
+    split_size = 50_000
+    split_tags = [all_player_tags[i:i + split_size] for i in range(0, len(all_player_tags), split_size)]
+
+    for count, group in enumerate(split_tags, 1):
+        t = time.time()
+        print(f"Group {count}/{len(split_tags)}")
+        docs_to_insert = []
+        previous_player_responses = await cache.mget(keys=group)
+        for response in previous_player_responses:
+            if response is not None:
+                response = orjson.loads(snappy.decompress(response))
+                d = {
+                    "name": response.get("name"),
+                    "clan": response.get("clan", {}).get("tag", "No Clan"),
+                    "league": response.get("league", {}).get("name", "Unranked"),
+                    "id": response.get("tag").replace("#", ''),
+                    "th": response.get("townHallLevel"),
+                    "clan_name": response.get("clan", {}).get("name", "No Clan"),
+                    "trophies": response.get("trophies")
                 }
-            },
-            {"$unset": ["_id"]}
-        ]
-        docs_to_insert = await db_client.global_clans.aggregate(pipeline=pipeline).to_list(length=None)
-        logger.info(f"{len(docs_to_insert)} docs")
-
-        # An index is where the documents are stored.
+                docs_to_insert.append(d)
+        print(f"starting bulk write: took {time.time() - t} secs")
 
         async def add_documents(documents):
             headers = {"Authorization" : f"Bearer {config.meili_pw}"}
@@ -51,23 +63,4 @@ async def main():
             tasks.append(asyncio.create_task(add_documents(documents=doc_group)))
         await asyncio.gather(*tasks)
 
-        done = False
-        while not done:
-            await asyncio.sleep(60)
-            try:
-                headers = {"Authorization": f"Bearer {config.meili_pw}"}
-                async with aiohttp.ClientSession() as session:
-                    async with session.get('http://85.10.200.219:7700/tasks?limit=1', headers=headers) as response:
-                        if response.status == 200:
-                            data = await response.json()
-                            results = data.get("results", [])
-                            if results[0].get("status") == "succeeded":
-                                done = True
-                            else:
-                                continue
-                        else:
-                            continue
-            except Exception:
-                continue
-
-        await asyncio.sleep(60)
+        await asyncio.sleep(15)
