1717bad09535408fa277de4359bae354fdca50ba
MagicTheDev <82341152+MagicTheDev@users.noreply.github.com>
2024-12-14 16:47:10 -0600

    Update track.py

--- bot/giveaway/track.py
+++ bot/giveaway/track.py
@@ -93,11 +93,3 @@ async def main():
     finally:
         logger.info("Shutting down Kafka producer...")
         await producer.stop()
-
-
-# Run the script
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except Exception as e:
-        logger.critical(f"Critical error while running the tracker: {e}")
